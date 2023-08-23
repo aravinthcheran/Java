265e31cf84089eabed440a8594ea0f6ae37e052e
Aravinth Cheran <115545089+aravinthcheran@users.noreply.github.com>
2023-08-23 14:01:52 +0000

    hi

---
+++ test/hello.java
@@ -0,0 +1,7 @@
+public class hello 
+{
+    public static void main(String a[]) 
+    {
+        System.out.print("Hello World!, This is my first java program.");
+    }
+}
---
+++ test/num.java
@@ -0,0 +1,22 @@
+class num
+{
+    public static void main(String a[])//sensitive to case
+    {
+        int num=4;// declare and initialize
+        int result=num+17;
+        double num1=198.5;
+        //to declare a float, use float num1=198.5f;
+        char c ='k';//single quotes for char, double quotes for string
+        byte b=127;//range -128 to 127
+        long l=1234567890l;//end with l
+        boolean d=true;//true or false
+
+        // literals
+        int num2=0b100;//binary
+        int num3=010;//octal
+        int num4=0x10;//hexadecimal
+        char e='a';
+        e++;//increments value by 1 and assign it to e as'b'
+        
+    }
+}
